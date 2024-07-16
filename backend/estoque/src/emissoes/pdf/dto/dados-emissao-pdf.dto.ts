9944e50b8654bf8ddbf8a839062f8b5795a5b920
gleycykelly <gleycy_amaro@hotmail.com>
2024-07-16 13:34:11 -0300

    Remove opção de emitir em PDF

--- backend/estoque/src/emissoes/pdf/dto/dados-emissao-pdf.dto.ts
+++
@@ -1,7 +0,0 @@
-import { IsNotEmptyObject } from 'class-validator';
-import { Depositos } from 'src/depositos/entities/deposito.entity';
-
-export class DadosEmissaoPdfDto {
-  @IsNotEmptyObject()
-  deposito: Depositos;
-}
